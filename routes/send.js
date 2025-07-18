4c5492445a29770590b35e47d9d3e1b6d6673c42
rachel <16939591+rkaufman13@users.noreply.github.com>
2025-07-17 22:09:43 -0400

    migrate to mailersend

--- routes/send.js
+++ routes/send.js
@@ -1,42 +1,54 @@
 var express = require("express");
-const sgMail = require("@sendgrid/mail");
+
+const MailerSend = require("mailersend").MailerSend;
+const Sender = require("mailersend").Sender;
+const Recipient = require("mailersend").Recipient;
+const EmailParams = require("mailersend").EmailParams;
 require("dotenv").config();
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const mailerSend = new MailerSend({ apiKey: process.env.MAILERSEND_API_KEY });
+const sentFrom = new Sender("noodle@noodleapp.cool", "Noodle");
 
 var router = express.Router();
 
-const sendGridTemplates = {
-  eventCreated: "d-1c0fd3f9eb674287b5eb839deb958cf0",
-  eventRsvped: "d-6e89baf471ab478682b2757a40fbe4fe",
+const mailerSendTemplates = {
+  eventCreated: "0p7kx4xqnrvg9yjr",
+  eventRsvped: "0r83ql3jvdxgzw1j",
 };
 
 /* POST to send email to event creator on event creation. */
 router.post("/", async function (req, res, next) {
   const vars = req.body;
 
-  const params = {
-    from: {
-      email: "noodle@noodleapp.cool",
-    },
-    personalizations: [
-      {
-        to: [{ email: vars.hostEmail }],
-        dynamic_template_data: {
-          name: vars.hostName,
-          eventName: vars.eventName,
-          eventAdminUrl: vars.fullURL,
-        },
+  const recipient = [new Recipient(vars.hostEmail, vars.hostName)];
+
+  const personalization = [
+    {
+      email: vars.hostEmail,
+      data: {
+        name: vars.hostName,
+        eventName: vars.eventName,
+        eventAdminUrl: vars.fullURL,
       },
-    ],
-    template_id: sendGridTemplates.eventCreated,
-  };
+    },
+  ];
+  const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipient)
+    .setReplyTo(sentFrom)
+    .setSubject(`Your Nood, ${vars.eventName}, has been created`)
+    .setTemplateId(mailerSendTemplates.eventCreated)
+    .setPersonalization(personalization);
 
   try {
-    const data = await sgMail.send(params);
-    console.log(data);
-    res.status(200).send();
+    mailerSend.email
+      .send(emailParams)
+      .then((response) => {
+        res.status(200).send();
+      })
+      .catch((err) => res.status(500).send(err.message));
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(err.message);
   }
 });
 
@@ -44,27 +56,36 @@ router.post("/", async function (req, res, next) {
 router.post("/rsvp", async function (req, res, next) {
   const vars = req.body;
 
-  const params = {
-    from: {
-      email: "noodle@noodleapp.cool",
-    },
-    personalizations: [
-      {
-        to: [{ email: vars.hostEmail }],
-        dynamic_template_data: {
-          name: vars.hostName,
-          eventName: vars.eventName,
-          eventAdminUrl: vars.fullURL,
-          respondee: vars.respondee,
-        },
+  const recipient = [new Recipient(vars.hostEmail, vars.hostName)];
+
+  const personalization = [
+    {
+      email: vars.hostEmail,
+      data: {
+        name: vars.hostName,
+        eventName: vars.eventName,
+        eventAdminUrl: vars.fullURL,
+        respondee: vars.respondee,
       },
-    ],
-    template_id: sendGridTemplates.eventRsvped,
-  };
+    },
+  ];
+
+  const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipient)
+    .setReplyTo(sentFrom)
+    .setSubject(`${vars.respondee} just responded to your Nood`)
+    .setTemplateId(mailerSendTemplates.eventRsvped)
+    .setPersonalization(personalization);
 
   try {
-    const data = await sgMail.send(params);
-    res.status(200).send();
+    await mailerSend.email
+      .send(emailParams)
+      .then((response) => {
+        console.log(response);
+        res.status(200).send();
+      })
+      .catch((err) => res.status(500).send(err.message));
   } catch (error) {
     res.status(500).send(error.message);
   }
